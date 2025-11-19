7aa2e33af75791edcab090bca480c2e0f0220519
not-manu <manu.anish@mail.utoronto.ca>
2025-11-19 00:25:56 -0500

    update migrations

---
+++ drizzle/0000_tough_leader.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `settings` (
+	`id` integer PRIMARY KEY DEFAULT 0 NOT NULL,
+	`preferred_name` text NOT NULL,
+	`api_key` text,
+	`api_key_type` text,
+	`created_at` integer,
+	`updated_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `folders` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`path` text NOT NULL,
+	`name` text NOT NULL,
+	`is_favorite` integer DEFAULT false,
+	`created_at` integer,
+	`updated_at` integer,
+	`last_accessed_at` integer
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `folders_path_unique` ON `folders` (`path`);--> statement-breakpoint
+CREATE TABLE `chat` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`created_at` integer,
+	`updated_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `message` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`chat_id` integer NOT NULL,
+	`role` text NOT NULL,
+	`content` text NOT NULL,
+	`created_at` integer,
+	FOREIGN KEY (`chat_id`) REFERENCES `chat`(`id`) ON UPDATE no action ON DELETE cascade
+);
